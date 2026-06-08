e147abce991d4d24566879ddbf8bcd82bfcdf510
rhkdgml999-lang <rhkdgml999@dongyang.ac.kr>
2026-06-08 11:09:31 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'rg-robotics-v1';
+const CACHE_NAME = 'rg-robotics-v2';
 const urlsToCache = [
   './',
   './index.html',
@@ -12,6 +12,22 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting(); // 즉시 활성화를 유도
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
 
